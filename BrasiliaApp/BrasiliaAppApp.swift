ddf066121aa4f233e467a32239ad80afbf7b3ea1
Yago Marques <yagovictormarques@gmail.com>
2023-10-11 20:00:48 -0300

    Initial Commit

---
+++ BrasiliaApp/BrasiliaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrasiliaAppApp.swift
+//  BrasiliaApp
+//
+//  Created by user on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BrasiliaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
