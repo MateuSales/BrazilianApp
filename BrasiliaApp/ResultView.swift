71932aba04103f0d9f0017a98db3c6c1ed53d3cb
MateuSales <matheussalles40@gmail.com>
2023-10-13 13:50:06 -0300

    first commit

---
+++ BrasiliaApp/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  BrasiliaApp
+//
+//  Created by user on 11/10/23.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# BrazilianApp
